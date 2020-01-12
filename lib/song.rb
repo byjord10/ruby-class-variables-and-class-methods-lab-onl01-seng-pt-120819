34c28e240a8f189ae1756abb338b4c3d9222374d
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-12 21:16:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,12 +42,13 @@ count = []
 
   def self.genre_count
     genre_count = {}
-    @@genre.each do |genre|
-      if genre_count|genre|
-        genre_count|genre| +=1
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
       else
-        genre_count|genre| =1
+        genre_count[genre] = 1
       end
     end
     genre_count
   end
+end
