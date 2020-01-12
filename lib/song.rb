6c41ec9c5228d5eae39c9cccb97519732df0de8a
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-12 21:15:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,9 +44,9 @@ count = []
     genre_count = {}
     @@genre.each do |genre|
       if genre_count|genre|
-        genre_count|genre| += 1
+        genre_count|genre| +=1
       else
-        genre_count|genre| = 1
+        genre_count|genre| =1
       end
     end
     genre_count
